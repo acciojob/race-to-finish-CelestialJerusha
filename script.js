79af6c83d28d250df9a9f37370d58dda148e90bb
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-07-26 20:41:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+let promises = ["a","b","c","d","e"];
+Promise.any(promises).then((value) => console.log(value));
