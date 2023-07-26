ff628d237e4a8129976b926cc9381a5dde553011
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-07-26 21:04:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,19 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
-let promises = ["a","b","c","d","e"];
+const promises = [p1,p2,p3,p4,p5];
+const p1 = new Promise((resolve) => setTimeout(() => {  
+console.log('frst Promise is fulfilled');   
+}, 1000));
+const p2 = new Promise((resolve) => setTimeout(() => {  
+console.log('second Promise is fulfilled');   
+}, 2000));
+const p3 = new Promise((resolve) => setTimeout(() => {  
+console.log('third Promise is fulfilled'); 
+}, 3000))
+console.log('fourth Promise is fulfilled');   
+}, 4000));
+const p3 = new Promise((resolve) => setTimeout(() => {  
+console.log('fifth Promise is fulfilled'); 
+}, 5000))
 Promise.any(promises).then((value) => console.log(value));
