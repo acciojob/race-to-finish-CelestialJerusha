343bce4a1eb54dc6a157f57fbd0133b7d36169b4
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-07-26 21:24:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,19 +2,25 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
-const promises = [p1,p2,p3,p4,p5];
+const result = document.getElementById("output");
+
 const p1 = new Promise((resolve) => setTimeout(() => {  
-console.log('frst Promise is fulfilled');   
+result.innerHTML+="first promise fulfilled"   
 }, 1000));
 const p2 = new Promise((resolve) => setTimeout(() => {  
-console.log('second Promise is fulfilled');   
+result.innerHTML+="second promise fulfilled"   
 }, 2000));
 const p3 = new Promise((resolve) => setTimeout(() => {  
-console.log('third Promise is fulfilled'); 
-}, 3000))
-console.log('fourth Promise is fulfilled');   
+result.innerHTML+="third promise fulfilled" 
+}, 3000));
+const p4 = new Promise((resolve) => setTimeout(() => {  
+result.innerHTML+="fourth promise fulfilled"    
 }, 4000));
-const p3 = new Promise((resolve) => setTimeout(() => {  
-console.log('fifth Promise is fulfilled'); 
-}, 5000))
+const p5 = new Promise((resolve) => setTimeout(() => {  
+result.innerHTML+="fifth promise fulfilled"  
+}, 5000));
+
+
+
+const promises = [p1,p2,p3,p4,p5];
 Promise.any(promises).then((value) => console.log(value));
