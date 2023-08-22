dcc3ac62072620abe3d7afb941a19fd1f58d1b01
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-08-22 23:18:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,22 +5,22 @@ window.promises = [];
 const result = document.getElementById("output");
 
 const p1 = new Promise((resolve) => setTimeout(() => {  
-result.innerHTML+="first promise fulfilled"   
+result.innerHTML+="first promise fulfilled "   
 }, 1000));
 const p2 = new Promise((resolve) => setTimeout(() => {  
-result.innerHTML+="second promise fulfilled"   
+result.innerHTML+="second promise fulfilled "   
 }, 2000));
 const p3 = new Promise((resolve) => setTimeout(() => {  
-result.innerHTML+="third promise fulfilled" 
+result.innerHTML+="third promise fulfilled " 
 }, 3000));
 const p4 = new Promise((resolve) => setTimeout(() => {  
-result.innerHTML+="fourth promise fulfilled"    
+result.innerHTML+="fourth promise fulfilled "    
 }, 4000));
 const p5 = new Promise((resolve) => setTimeout(() => {  
-result.innerHTML+="fifth promise fulfilled"  
+result.innerHTML+="fifth promise fulfilled "  
 }, 5000));
 
 
 
 const promises = [p1,p2,p3,p4,p5];
-Promise.any(promises).then((value) => console.log(value));
+
